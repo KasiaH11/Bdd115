e51257df7b92d1c471c423d4129779b105490bc3
KasiaH11 <katarzyna.haladyniak.praca@onet.pl>
2022-12-07 21:10:14 +0100

    logowanie poprawne i niepoprawne tabela duzo przypadkow

--- src/test/java/Features/logowanie.feature
+++ src/test/java/Features/logowanie.feature
@@ -1,3 +1,4 @@
+@now
 Feature: Logowanie do aplikacji
 
   Jako uzytkownik chce miec funkcjonalnosc
@@ -5,19 +6,53 @@ Feature: Logowanie do aplikacji
   z zaawansowanych funkcji systemu
 
   Scenario: Poprawne logowanie do aplikacji
-    Given Uzytkownik otwiera przegladarke
-    And Uzytkownik wpisuje adres strony internetowej
+    Given Uzytkownik wpisuje adres strony internetowej
     When Uzytkownik wpisuje poprawny login
     And Uzytkownik wpisuje poprawen haslo
     And Uzytkownik klika przycisk Login
     Then Uzytkownik zostaje poprawnie zalogowany do aplikacji
 
 
-  Scenario: Poprawne logowanie do aplikacji
-    Given Uzytkownik otwiera przegladarke
-    And Uzytkownik wpisuje adres strony internetowej
+    Scenario: Poprawne logowanie do aplikacji - wersja druga
+      Given Uzytkownik przechodzi na strone "https://the-internet.herokuapp.com/login"
+      When Uzytkownik wprowadza login "tomsmith"
+      And Uzytkownik wprowadza haslo "SuperSecretPassword!"
+      And Uzytkownik klika przycisk Login
+      Then Uzytkownik zostaje poprawnie zalogowany do aplikacji
+
+
+
+  Scenario: Niepoprawne logowanie do aplikacji - wersja druga
+    Given Uzytkownik przechodzi na strone "https://the-internet.herokuapp.com/login"
+    When Uzytkownik wprowadza login "tomsmith"
+    And Uzytkownik wprowadza haslo "blablabla"
+    And Uzytkownik klika przycisk Login
+    Then Uzytkownik nie zostaje poprawnie zalogowany do aplikacji
+
+  Scenario: Niepoprawne logowanie do aplikacji - wersja trzecia
+    Given Uzytkownik przechodzi na strone "https://the-internet.herokuapp.com/login"
+    When Uzytkownik wprowadza login "blablabla"
+    And Uzytkownik wprowadza haslo "SuperSecretPassword!"
+    And Uzytkownik klika przycisk Login
+    Then Uzytkownik nie zostaje poprawnie zalogowany do aplikacji
+
+  Scenario: Niepoprawne logowanie do aplikacji
+    Given Uzytkownik wpisuje adres strony internetowej
     When Uzytkownik wpisuje poprawny login
     And Uzytkownik wpisuje niepoprawen haslo
     And Uzytkownik klika przycisk Login
     Then Uzytkownik nie zostaje poprawnie zalogowany do aplikacji
 
+    @now
+    Scenario Outline: Niepoprawene logowanie - wersja cztery
+      Given Uzytkownik przechodzi na strone "https://the-internet.herokuapp.com/login"
+      When Uzytkownik wprowadza loginek <loginek>
+      And Uzytkownik wprowadza haslko <haselko>
+      And Uzytkownik klika przycisk Login
+      Then Uzytkownik nie zostaje poprawnie zalogowany do aplikacji
+
+      Examples:
+      | loginek | haselko |
+      | tomsmith | blablabla |
+      | blablabla | SuperSecretPassword! |
+      | blablabla | blablabla |
--- src/test/java/Steps/Logowanie.java
+++ src/test/java/Steps/Logowanie.java
@@ -1,23 +1,32 @@
 package Steps;
 
+import io.cucumber.java.AfterAll;
+import io.cucumber.java.BeforeAll;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Logowanie {
 
-    WebDriver driver;
+   private static WebDriver driver;
 
+    @BeforeAll
+    public static void setDriver() {
+        //otwarcie przegladarki
+        driver = new ChromeDriver();//konstruktor wywolujacy przegladarke;
+    }
 
     @Given("Uzytkownik otwiera przegladarke")
     public void uzytkownik_otwiera_przegladarke() {
         System.out.println("Krok 1 - Uzytkownik otwiera przegladarke");
-        driver = new ChromeDriver();//konstruktor wywolujacy przegladarke;
-        driver.manage().window().maximize();
+       // driver = new ChromeDriver();//konstruktor wywolujacy przegladarke;
+      // driver.manage().window().maximize();
     }
 
     @Given("Uzytkownik wpisuje adres strony internetowej")
@@ -29,7 +38,10 @@ public class Logowanie {
     @When("Uzytkownik wpisuje poprawny login")
     public void uzytkownik_wpisuje_poprawny_login() {
         System.out.println("Krok 3 - Uzytkownik wpisuje poprawny login");
-        driver.findElement(By.id("username")).sendKeys("tomsmith");
+        WebElement usernameInput = driver.findElement(By.id("username"));
+        usernameInput.click();
+        usernameInput.sendKeys("tomsmith");
+        //driver.findElement(By.id("username")).sendKeys("tomsmith");
 
     }
 
@@ -43,15 +55,22 @@ public class Logowanie {
     @When("Uzytkownik klika przycisk Login")
     public void uzytkownik_klika_przycisk_login() {
         System.out.println("Krok 5 - Uzytkownik klika przycisk Login");
-        driver.findElement(By.tagName("button")).click();
 
+        WebElement loginButton = driver.findElement(By.tagName("button"));
+        String textButton = loginButton.getText();
+        System.out.println("PRZYCISK LOGIN ZAWIERA TEKST = " + textButton);
+        loginButton.click();
+
+        // driver.findElement(By.tagName("button")).click();
+
+        //driver.???
     }
 
     @Then("Uzytkownik zostaje poprawnie zalogowany do aplikacji")
     public void uzytkownik_zostaje_poprawnie_zalogowany_do_aplikacji() {
         System.out.println("Krok 6 - Uzytkownik zostaje poprawnie zalogowany do aplikacji");
         Assert.assertEquals("https://the-internet.herokuapp.com/secure", driver.getCurrentUrl());
-        driver.close();
+        //driver.close();
 
     }
     @When("Uzytkownik wpisuje niepoprawen haslo")
@@ -64,7 +83,41 @@ public class Logowanie {
     @Then("Uzytkownik nie zostaje poprawnie zalogowany do aplikacji")
     public void uzytkownik_nie_zostaje_poprawnie_zalogowany_do_aplikacji() {
         Assert.assertEquals("https://the-internet.herokuapp.com/login", driver.getCurrentUrl());
-        driver.close();
+        //driver.close();
     }
 
+   // And Uzytkownik przechodzi na strone "https://the-internet.herokuapp.com/login"
+    @Given("Uzytkownik przechodzi na strone {string}")
+    public void uzytkownik_przechodzi_na_strone(String adresStrony) {
+        //adresStrony = "https://the-internet.herokuapp.com/login"
+        driver.navigate().to(adresStrony);
+    }
+    // When Uzytkownik wprowadza login "tomsmith"
+    @When("Uzytkownik wprowadza login {string}")
+    public void uzytkownik_wprowadza_login(String userName) {
+        driver.findElement(By.id("username")).sendKeys(userName);
+
+    }
+    //And Uzytkownik wprowadza haslo "SuperSecretPassword!"
+    @When("Uzytkownik wprowadza haslo {string}")
+    public void uzytkownik_wprowadza_haslo(String password) {
+        driver.findElement(By.name("password")).sendKeys(password);
+
+    }
+
+    //When Uzytkownik wprowadza login <loginek>
+    @When("^Uzytkownik wprowadza loginek (.+)$")
+    public void uzytkownik_wprowadza_login2(String loginek) {
+        driver.findElement(By.id("username")).sendKeys(loginek);
+    }
+
+    @And("^Uzytkownik wprowadza haslko (.+)$")
+    public void uzytkownik_wprowadza_haslo2(String haselko) {
+        driver.findElement(By.name("password")).sendKeys(haselko);
+    }
+
+    @AfterAll
+    public static void tearDriver(){
+        driver.close();
+    }
 }
